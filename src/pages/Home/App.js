85479a5a85ac8488e2c5df0314425d45d57dbce5
cbartram <cbartram3@gmail.com>
2019-07-25 18:35:48 -0400

    Added part 3

--- src/pages/Home/App.js
+++ src/pages/Home/App.js
@@ -1,17 +1,55 @@
 import React, { Component } from 'react';
+import { Card, Image, Icon } from 'semantic-ui-react';
 import withContainer from '../../components/withContainer/withContainer';
+import { API_URL } from "../../constants";
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      images: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(`${API_URL}?query=ocean`, {
+      headers: {
+        Authorization: `Client-ID 51d4e3876e7e846aefdc9c0db737f7ab3dc5034b59fda2cbd5d07e2deeac1e3b`
+      }
+    })
+      .then(res => res.json())
+      .then(res => this.setState({ images: res.results }))
   }
 
   render() {
     return (
-        <h1>Home</h1>
+        <div>
+          <h1>Home</h1>
+          <Card.Group itemsPerRow={3}>
+          {
+            this.state.images.map(item => {
+              return (
+                  <Card >
+                    <Image size="medium" src={item.urls.regular} wrapped ui={false} />
+                    <Card.Content>
+                      <Card.Header>{item.title}</Card.Header>
+                      <Card.Meta>
+                        <span className='date'>
+                          { item.created_at}
+                        </span>
+                      </Card.Meta>
+                      <Card.Description>
+                        { item.description + '' + item.alt_description }
+                      </Card.Description>
+                    </Card.Content>
+                  </Card>
+              )
+            })
+          }
+          </Card.Group>
+        </div>
     )
   }
 }
