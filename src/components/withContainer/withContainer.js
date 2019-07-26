3cf651127e3f28f70304701ab96f11eb245b32ac
cbartram <cbartram3@gmail.com>
2019-07-26 11:34:41 -0400

    Updated part 1 for photos

--- src/components/withContainer/withContainer.js
+++ src/components/withContainer/withContainer.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './withContainer.css';
 import Navbar from "../Navbar/Navbar";
-import Sidebar from '../Sidebar/Sidebar';
 import { Container } from "semantic-ui-react";
 
 
@@ -9,18 +8,14 @@ import { Container } from "semantic-ui-react";
  * HOC wrapping a base component with a provided container, sidebar, and navbar
  * @param BaseComponent
  * @param navbarProps
- * @param sidebarProps
- * @returns {{new<P, S>(props: Readonly<P>): Enhanced, new<P, S>(props: P, context?: any): Enhanced, prototype: Enhanced}}
  */
-const withContainer = (BaseComponent, navbarProps, sidebarProps) => {
+const withContainer = (BaseComponent, navbarProps) => {
   return class Enhanced extends Component {
     render() {
         return (
             <Container fluid>
               <Navbar {...navbarProps} />
-              <Sidebar {...sidebarProps} >
-                <BaseComponent/>
-              </Sidebar>
+              <BaseComponent/>
             </Container>
         )
     }
